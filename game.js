911450aa918454829ceafdac21f9a2d9926c3a02
Jacob <gruvinbro@gmail.com>
2020-12-10 16:53:39 -0700

    changes

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
+const maxItems = 4;
+
 let Zero = {
     name: "Zero",
+    maxHealth: 100,
     health: 100,
     hits: 0,
     attacks: {
@@ -15,9 +18,12 @@ let One = {
     health: 88,
     hits: 0,
     attacks: {
-        kick: 15,
-        punch: 6, 
-        slap: 2
+        kickMin: 12,
+        kickMax: 15,
+        punchMin: 2,
+        punchMax: 4,
+        slapMin: 1, 
+        slapMax: 3
     },
     items: []
 };
@@ -28,24 +34,41 @@ let items ={
      metal: {name: "Iron",modifier: 3,description: "Yikes!"}
 };
 
+function randomNumber(min, max){
+    const r = Math.random()*(max-min) + min
+    return Math.floor(r)
+}
+
 
 
 function slap(){
-    Zero.health -= (One.attacks.slap + addMods());
+    Zero.health -= ( randomNumber(One.attacks.slapMin, One.attacks.slapMax) + addMods());
     Zero.hits++;
+    if(Zero.health <= 0){
+        defeated();
+
+    }
     update();
 
 }
 
 function punch(){
-    Zero.health -= (One.attacks.punch + addMods());
+    Zero.health -= (randomNumber(One.attacks.punchMin, One.attacks.punchMax) + addMods());
     Zero.hits++;
+    if(Zero.health <= 0){
+        defeated();
+
+    }
     update();
 }
 
 function kick(){
-    Zero.health -= (One.attacks.kick + addMods());
+    Zero.health -= (randomNumber(One.attacks.kickMin, One.attacks.kickMax) + addMods());
     Zero.hits++;
+    if(Zero.health <= 0){
+        defeated();
+
+    }
     update();
 }
 
@@ -53,28 +76,106 @@ function update(){
     document.getElementById("health").innerText = Zero.health; 
     document.getElementById("name").innerText = Zero.name;
     document.getElementById("hits").innerText = Zero.hits;
+
+    let percentHealth = (Zero.health / Zero.maxHealth) * 100;
+
+    document.getElementById("health-percent").style.width = percentHealth + "%";
 }
 
 function giveFire(){
-    Zero.items.push(items.fire);
+    if(One.items.length < maxItems){
+        One.items.push(items.fire);
+    } 
+    if (One.items.length == maxItems) {
+        togglePowerUps(true);
+    }
 }
 
 function giveWater(){
-    Zero.items.push(items.water);
+    if(One.items.length < maxItems){
+        One.items.push(items.water);
+    } 
+    if (One.items.length == maxItems) {
+        togglePowerUps(true);
+    }
 }
 
 function giveMetal(){
-    Zero.items.push(items.metal);
+    if(One.items.length < maxItems){
+        One.items.push(items.metal);
+    } 
+    if (One.items.length == maxItems) {
+        togglePowerUps(true);
+    }
 }
 
 function addMods(){
     let totalMod = 0;
-    for (let index = 0; index < Zero.items.length; index++) {
+    for (let index = 0; index < One.items.length; index++) {
         
-         totalMod += Zero.items[index].modifier;
+         totalMod += One.items[index].modifier;
         
     }
     return totalMod;
 }
 
+function togglePowerUps(turnOff){
+    if(turnOff){
+        document.getElementById("fire").disabled = true;
+        document.getElementById("water").disabled = true;
+        document.getElementById("metal").disabled = true;
+    }else{
+        document.getElementById("fire").disabled = false;
+        document.getElementById("water").disabled = false;
+        document.getElementById("metal").disabled = false;
+
+    }
+
+
+}
+
+function togglePowerIcons() {
+    if(){
+        document.getElementById("fire-icon").disabled = true;
+        document.getElementById("water-icon").disabled = true;
+        document.getElementById("metal-icon").disabled = true;
+    }else{
+        document.getElementById("fire-icon").disabled = false;
+        document.getElementById("water-icon").disabled = false;
+        document.getElementById("metal-icon").disabled = false;
+
+    }
+
+
+    
+}
+
+function defeated() {
+    Zero.health = 0;
+
+    document.getElementById("slap").disabled = true;
+    document.getElementById("punch").disabled = true;
+    document.getElementById("kick").disabled = true;
+    
+    let theEnd = document.getElementById("message")
+    theEnd.innerText = "GAME OVER"
+    setTimeout(() => { 
+        resetGame();
+        theEnd.innerText = "";
+
+        document.getElementById("slap").disabled = false;
+        document.getElementById("punch").disabled = false;
+        document.getElementById("kick").disabled = false;
+    }, 5000);
+}
+
+function resetGame(){
+    if(One.items.length == maxItems){
+        togglePowerUps(false);
+    }
+    Zero.health = 100;
+    Zero.hits = 0;
+    One.items = [];
+    update();
+}
 update();
